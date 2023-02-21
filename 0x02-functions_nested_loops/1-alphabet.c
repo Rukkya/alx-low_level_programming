7bacd7ab26c249840f2ceef5c45f1868e27fddd1
rukkya <rokiaossama@gmail.com>
2023-02-21 15:09:22 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ *
+ * Return: void.
+ */
+int print_alphabet(void)
+{
+	char c;
+	for(c='a';c<='z';c++)
+_putchar(c);
+_putchar(\n);
+    return (0);
+}
