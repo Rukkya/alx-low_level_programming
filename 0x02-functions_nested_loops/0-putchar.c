fc621d0c70a1a791bd9da3881f5dfd89c717ec27
rukkya <rokiaossama@gmail.com>
2023-02-21 14:50:47 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,9 @@
 #include"main.h"
-/** * main - prints the word _putchar * Return: return 0 */
+
+/** 
+ * main - prints the word _putchar 
+ * Return: return 0
+ */
 int main(void)
 {
 _putchar('_');
