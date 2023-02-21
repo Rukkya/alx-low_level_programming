dc490edc49b4ad452bea1757ee41b2e210d224d5
rukkya <rokiaossama@gmail.com>
2023-02-21 14:17:59 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
 #include"main.h"
-/* main - entry point
+/* 
+ * main - entry point
  * Return: always 0.
+ *
  */
-int main(void)
+int main(void) /*
+		* to print _putchar on screen
+		*/
 {
-_putchar('-');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
