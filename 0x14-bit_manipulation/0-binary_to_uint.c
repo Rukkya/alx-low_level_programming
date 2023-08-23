08da972d644189637175cedad06f1505311e6cb3
rukkya <rokiaossama@gmail.com>
2023-08-22 17:26:28 -0700

    ll

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "0-main.c"
+/**
+  * conversion - converts chars to integers
+  * @c: char to be converted
+  * Return: converted integer
+  */
+unsigned int conversion(char c)
+{
+    return ((unsigned int) c - '0');
+}
+/**
+  * _strlen - calculates the length of the string
+  * @input: input
+  * Return: length of string
+  */
+unsigned int _strlen(const char *input)
+{
+    int i;
+    for (i = 0; input[i]; i++){
+    return (i);}
+    return(0);
+}
+/**
+  * binary_to_uint - converts a string which is binary num basically to a decimal integere output
+  * @bin: the binary number
+  * Return: unsigned deci number
+  */
+unsigned int binary_to_uint(const char *binn)
+{
+    int i;
+    unsigned int deci, var1, exp;
+
+    if (!binn)
+        return (0);
+    deci = var1 = 0;
+    exp = 1;
+    for (i = _strlen(binn) - 1; binn[i]; i--, exp *= 2)
+    {
+        if (binn[i] != '0' && binn[i] != '1')
+            return (0);
+        var1 = conversion(binn[i]);
+        deci += var1 * exp;
+    }
+    return (deci);
+}
