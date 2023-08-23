468d4af1356237e8b32088b5bba52f788484e9b7
rukkya <rokiaossama@gmail.com>
2023-08-22 18:25:23 -0700

    l

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,19 +1,13 @@
 #include "main.h"
 /**
-  * print_binary - converts a decimal number to binary
-  * @n: integer to be  converted
-  */
-void print_binary(unsigned long int xn)
+ * print_binary - prints the binary representation of a number
+ * @num: unsigned long int to be printed in binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int num)
 {
-    int var, flag;
-
-    if (xn == 0)
-        _putchar('0');
-    for (flag = 0, var = sizeof(xn) * 8 - 1; var >= 0; var--)
-    {
-        if ((xn >> var) & 1)
-            flag = 1;
-        if (flag == 1)
-            ((xn >> var) & 1) ? _putchar('1') : _putchar('0');
-    }
+    if (num > 1)
+        print_binary(num >> 1);
+    putchar((num & 1) + '0');
 }
